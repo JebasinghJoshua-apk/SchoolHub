005aaf3fcc73007d7d82a947be0e2707d9bd8765
jebasingh-joshua <jeb@friendlycares.com>
2024-05-31 11:16:57 +0530

    Add project files.

---
+++ SchoolHub.API/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using SchoolHub.API.Model;
+using System.Collections.Generic;
+
+namespace SchoolHub.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        [Route("GetStudentListByClass/{className}")]
+        public List<StudentModel> GetStudentListByClass(string className)
+        {
+            var studentList = new List<StudentModel>();
+
+            var connectionString = "Server=tcp:schoolhub.database.windows.net,1433;Initial Catalog=school_hub;Persist Security Info=False;User ID=schooladmin;Password=Password123#;TrustServerCertificate=False;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            var sqlQuery = @"SELECT rollnumber,NAME,gender,
+                                    bloodgroup,fathername,contactnumber1,
+                                    class.class
+                            FROM    student
+                                    JOIN class
+                                      ON student.classid = class.id
+                            WHERE   class.class = '" + className + "'";
+            SqlCommand cmd = new SqlCommand(sqlQuery, connection);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                var student = new StudentModel()
+                {
+                    RollNumber = reader.GetString(0),
+                    StudentName = reader.GetString(1),
+                    Gender = reader.GetString(2),
+                    BloodGroup = reader.GetString(3),
+                    FatherName = reader.GetString(4),
+                    ContactNumber = reader.GetString(5)
+                };
+                studentList.Add(student);
+            }
+            return studentList;
+        }
+    }
+}
---
+++ SchoolHub.API/Model/StudentModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolHub.API.Model
+{
+    public class StudentModel
+    {
+        public string RollNumber { get; set; }
+        public string StudentName { get; set; }
+        public string Gender { get; set; }
+        public string BloodGroup { get; set; }
+        public string FatherName { get; set; }
+        public string ContactNumber { get; set; }
+    }
+}
